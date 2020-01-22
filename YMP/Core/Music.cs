cdb2bba612732670dcba33a8a0c51161366aa4ce
AlphaBs <ksi123456ab@naver.com>
2020-01-22 15:43:51 +0900

    add Music and PlayList class

---
+++ YMP/Core/Music.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMP.Core
+{
+    public class Music
+    {
+        public string YoutubeID { get; private set; }
+        public string Title { get; private set; }
+        public string Artists { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public string Thumbnail { get; private set; }
+    }
+}
---
+++ YMP/Core/PlayList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMP.Core
+{
+    public class PlayList
+    {
+        public PlayList(string name, Music[] musics)
+        {
+            this.Name = name;
+            this.Musics = musics;
+            CurrentMusicIndex = 0;
+            Lenght = musics.Length;
+        }
+
+        public string Name { get; private set; }
+
+        public int Lenght { get; private set; }
+        public int CurrentMusicIndex { get; private set; }
+        public Music[] Musics { get; private set; }
+
+        public Music GetCurrentusic()
+        {
+            return Musics[CurrentMusicIndex];
+        }
+
+        public Music GetNextMusic()
+        {
+            if (CurrentMusicIndex >= Lenght)
+                CurrentMusicIndex = 0;
+            else
+                CurrentMusicIndex++;
+
+            return Musics[CurrentMusicIndex];
+        }
+
+        public Music GetPreviousMusic()
+        {
+            if (CurrentMusicIndex <= 0)
+                CurrentMusicIndex = Lenght - 1;
+            else
+                CurrentMusicIndex--;
+
+            return Musics[CurrentMusicIndex];
+        }
+    }
+}
