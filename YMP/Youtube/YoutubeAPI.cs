9e62462cec7471ae84fb9288f7e0be979ef92ae5
AlphaBs <ksi123456ab@naver.com>
2020-01-27 23:36:19 +0900

    add youtube api package

---
+++ YMP/Youtube/YoutubeAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMP.Youtube
+{
+    public class YoutubeAPI
+    {
+        // Please input your YouTube Data API KEY
+        private static readonly string ApiKey = YoutubeDataAPI.KEY;
+
+    }
+}
