c831f620d90628f69bb11ca2ccf4e0115a50122b
AlphaBs <ksi123456ab@naver.com>
2020-01-27 23:57:48 +0900

    add YoutubeAPI.Search

--- YMP/Youtube/YoutubeAPI.cs
+++ YMP/Youtube/YoutubeAPI.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +12,28 @@ namespace YMP.Youtube
     public class YoutubeAPI
     {
         // Please input your YouTube Data API KEY
-        private static readonly string ApiKey = YoutubeDataAPI.KEY;
+        private static readonly string Key = YoutubeDataAPI.KEY;
 
+        public YoutubeAPI()
+        {
+            Service = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = Key
+            });
+        }
+
+        public YouTubeService Service { get; private set; }
+
+        public SearchListResponse Search(string query, string pagetoken)
+        {
+            var q = Service.Search.List("id,snippet");
+            q.MaxResults = 10;
+            q.PageToken = pagetoken;
+            q.Q = query;
+            q.Type = "video,playlist";
+
+            var res = q.Execute();
+            return res;
+        }
     }
 }
