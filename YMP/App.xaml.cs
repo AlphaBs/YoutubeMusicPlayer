ff957bbc5a1916addd89f23b7a1a8d3f0a8b84a7
AlphaBs <ksi123456ab@naver.com>
2020-01-28 23:33:37 +0900

    check mutex to prevent duplicated execution

--- YMP/App.xaml.cs
+++ YMP/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using YMP.Model;
@@ -15,8 +16,19 @@ namespace YMP
     /// </summary>
     public partial class App : Application
     {
+        public static Mutex CurrentMutex;
+        const string mutexId = "AE805CDF-D5FA-49F4-B679-80E70183284C";
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            bool isSuccess;
+            CurrentMutex = new Mutex(true, mutexId, out isSuccess);
+            if (!isSuccess)
+            {
+                MessageBox.Show("Youtube Music Player 가 이미 실행중입니다.");
+                Environment.Exit(0);
+            }
+
             Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
             YMPCore.Initialize();
         }
